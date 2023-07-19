3ca046f4618ad39d3ef7a52329e0f7a6b6bacf85
freshgum <freshgum.bubbles@protonmail.com>
2023-07-19 20:36:57 +0100

    feat(*): create barrel file

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { useContainer } from './hooks/use-container/use-container';
+export { useManyServices, useManyServicesOrNull } from './hooks/use-many-services/use-many-services';
+export { useService, useServiceOrNull } from './hooks/use-service/use-service';
+export { useTreeVisitor } from './hooks/use-tree-visitor/use-tree-visitor';
